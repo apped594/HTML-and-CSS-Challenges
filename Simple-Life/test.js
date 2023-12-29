adfdcd209281add30005a7ddb6b18b54d64c4c8c
Jonathan <jonathan@d730.co.za>
2023-12-29 09:48:17 +0200

    DOM traversal

--- Simple-Life/test.js
+++ Simple-Life/test.js
@@ -14,13 +14,13 @@ for (let i = 0; i < newItems.length; i++) {
     newItems[i].style.color = "blue";
 }
 
-//Creating Elements
+//Creating Elements - will not show something
 
 const navie = document.querySelector('.nav-list');
 const navieItem = document.createElement("li");
 const anchor = document.createElement('a');
 
-//Adding Elements
+//Adding Elements - this will actually add the elements to the DOM
 
 navie.append(navieItem)
 navieItem.append(anchor)
@@ -41,3 +41,34 @@ console.log(anchor.classList.contains('nav-link')) // should return false
 //Remove Element
 
 navieItem.remove();
+
+///Traverse the DOM
+
+//Parent Node traversal traversing up the DOM tree
+
+    let navList = document.querySelector('ul');
+    console.log(navList.parentElement);
+    console.log(navList.parentNode.parentNode);
+
+
+
+//Child Node traversal traversing down the DOM tree
+
+let navSter = document.querySelector('nav');
+
+console.log(navSter.childNodes);
+//console.log(navSter.childNodes[1]);
+console.log(navSter.firstElementChild); // selects the first HTML element type node
+//console.log(navSter.lastChild);  //Selects the Last Node could be any node type
+console.log(navSter.children) // returns HTML collection of HTML element type nodes only
+navSter.childNodes[1].style.backgroundColor = "red"; // need to specifiy because the first node is text node becasue of indents in code (user generated nor JS generated)
+
+
+//Sibling Node traversal Nodes on the same tree level in the DOM
+
+let difSter = document.querySelector('div')
+console.log(difSter.childNodes);
+
+let navSib = document.querySelector('nav');
+console.log(navSib.previousElementSibling); //Selects only HTML type Nodes
+console.log(navSib.nextSibling); // selects any Node
